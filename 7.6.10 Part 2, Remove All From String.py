366fdd230f38b24e53286d2b91787210a05bdc32
Abuginn <arujansurendiran@gmail.com>
2024-12-15 19:37:18 -0500

    Update 7.6.10 Part 2, Remove All From String.py
    Correct version.
    
    Signed-off-by: Abuginn <arujansurendiran@gmail.com>

--- 7.6.10 Part 2, Remove All From String.py
+++ 7.6.10 Part 2, Remove All From String.py
@@ -1,11 +1,12 @@
-word = "bananas"
-letter = "na"
-
+# Correct version
 def remove_all_from_string(word, letter):
     while True:
-        index = word.find(letter)
-        if index == -1:
+        index = word.find(letter) 
+        if index == -1:  
             return word
-        word = word[:index] + word[index+len(letter):]
+        word = word[:index] + word[index + len(letter):]
 
-print (remove_all_from_string(word, letter))
+word = input("Enter the word: ")
+letters_to_remove = input("Enter the letters to remove: ")
+
+print("Result after removing all occurrences:", remove_all_from_string(word, letters_to_remove))
